12156d495fa10f347c6fd478212b45c5d8567faf
sitisyarafana <sitisyarafana07@gmail.com>
2025-12-15 12:08:22 +0800

    k

--- labs/lab02/exercise6/exercise6.py
+++ labs/lab02/exercise6/exercise6.py
@@ -15,4 +15,12 @@ def is_leap_year(year):
     """
     # TODO: Implement the leap year logic
     # Hint: Check divisibility in the correct order
-    pass
+    
+    divisible_by_4 = (year % 4) == 0
+    divisible_by_100 = (year % 100) == 0
+    divisible_by_400 = (year % 400) == 0
+
+    if divisible_by_4 and (not divisible_by_100 or divisible_by_400) :
+        return True
+    else:
+        return False
