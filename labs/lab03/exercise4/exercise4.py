fdabff624c47f443ea9e5f11e958587f824ff49a
sitisyarafana <sitisyarafana07@gmail.com>
2026-01-05 13:06:08 +0800

    lab3

--- labs/lab03/exercise4/exercise4.py
+++ labs/lab03/exercise4/exercise4.py
@@ -1,2 +1,12 @@
 def has_warming_trend(temps):
-    pass
+    
+    for i in range(len(temps) - 2):
+        
+        day1 = temps[i]
+        day2 = temps[i + 1]
+        day3 = temps[i + 2]
+
+        if day2 > day1 and day3 > day2:
+            return True
+        
+    return False
--- labs/lab03/exercise5/exercise5.py
+++ labs/lab03/exercise5/exercise5.py
@@ -1,5 +1,17 @@
 def get_position(cars, car_number):
-    pass
+    for i in range(len(cars)):
+        if cars[i] == car_number:
+            return i
+         
 
 def has_overtaken(before, after, car1, car2):
-    pass
+    record1_start = get_position(before, car1)
+    record2_start = get_position(before, car2)
+
+    record1_end = get_position(after, car1)
+    record2_end = get_position(after, car2)
+
+    if record1_start > record2_start and record1_end < record2_end :
+         return True
+    
+    return False
